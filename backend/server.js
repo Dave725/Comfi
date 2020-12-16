6f6b9e521e6d969358b3c1e5a6cd3398ae7130f7
David Wu <dswdavid1@gmail.com>
2020-12-16 17:20:09 +0000

    add .env and set up npm dev scripts

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-const express = require("express");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
 
+dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
@@ -16,6 +18,8 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen("5000", () => {
-  "Server started at port 5000";
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started in ${process.env.NODE_ENV} mode at port ${PORT}`);
 });
